f4919b1f3ef849ad0cd93768f8f4f1188294c096
Jakub Szal <jakubszalik@gmail.com>
2020-11-04 13:24:39 +0100

    Spring Initializr project created

---
+++ src/main/java/pl/jacobscarf/myorchestra/MyorchestraApplication.java
@@ -0,0 +1,13 @@
+package pl.jacobscarf.myorchestra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyorchestraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyorchestraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jacobscarf/myorchestra/MyorchestraApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jacobscarf.myorchestra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyorchestraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
