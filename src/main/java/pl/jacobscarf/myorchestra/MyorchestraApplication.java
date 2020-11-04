c083ee08fde879e6fee534b75706a50e67eced05
Jakub Szal <jakubszalik@gmail.com>
2020-11-04 13:44:04 +0100

    Added google code style

--- src/main/java/pl/jacobscarf/myorchestra/MyorchestraApplication.java
+++ src/main/java/pl/jacobscarf/myorchestra/MyorchestraApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyorchestraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyorchestraApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(MyorchestraApplication.class, args);
+  }
 }
